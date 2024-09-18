355e54889bb27acab69f0a16cabf40e5a6c33548
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2024-09-18 22:44:59 +0900

    use csv package

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Muhyun-Kim/go_sutdy
 
 go 1.19
+
+replace github.com/Muhyun-Kim/gocsvmapper => ../gocsvmapper
+
+require github.com/Muhyun-Kim/gocsvmapper v0.0.0
--- main.go
+++ main.go
@@ -1,67 +1,36 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
-	"html/template"
-	"io"
-	"net/http"
 	"os"
+
+	"github.com/Muhyun-Kim/gocsvmapper"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t := template.New("Upload template")
-		t, _ = t.Parse(uploadTemplate)
-		t.Execute(w, nil)
-	})
-
-	http.HandleFunc("/upload", uploadHandler)
-
-	fmt.Println("Starting server at :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Failed to start server:", err)
-		os.Exit(1)
-	}
-}
-
-var uploadTemplate = `
-<!DOCTYPE html>
-<html>
-<body>
-    <h2>CSV File Upload</h2>
-    <form enctype="multipart/form-data" action="/upload" method="post">
-        <input type="file" name="file" accept=".csv">
-        <input type="submit" value="Upload">
-    </form>
-</body>
-</html>
-`
-
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
+	// 사전에 정의된 컬럼 매핑
+	columnMap := map[string]string{
+		"학생": "student",
+		"나이": "age",
 	}
 
-	file, _, err := r.FormFile("file")
+	// CSV 파일 열기
+	file, err := os.Open("./example.csv") // gocsvmapper 폴더에 있는 example.csv 경로
 	if err != nil {
-		http.Error(w, "Error uploading file", http.StatusInternalServerError)
+		fmt.Println("Error opening file:", err)
 		return
 	}
 	defer file.Close()
 
-	reader := csv.NewReader(file)
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			http.Error(w, "Error reading file", http.StatusInternalServerError)
-			return
-		}
-		fmt.Println(record)
-		fmt.Fprintln(w, record)
+	// CSV 파일 처리
+	processor := gocsvmapper.NewCSVColumnProcessor(columnMap)
+	records, err := processor.MapCSVColumns(file)
+	if err != nil {
+		fmt.Println("Error processing CSV:", err)
+		return
 	}
+
+	// 처리된 CSV를 문자열로 출력
+	csvString := gocsvmapper.CSVToString(records)
+	fmt.Println(csvString)
 }
